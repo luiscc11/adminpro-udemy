820b49644131c019e9a4415ff502261cfbf6244a
luis casillas <luis_cc11@hotmail.com>
2020-01-14 22:05:30 -0600

    Agragar modulos y rutas

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+
+// modules
+import { SharedModule } from '../shared/shared.module';
+
+// routes
+import { PAGES_ROUTES } from './pages.routes';
+
+// components
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule { }
